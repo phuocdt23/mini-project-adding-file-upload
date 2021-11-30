f5016d65e520001aee05cbc442b3ba8816dc940c
Phuoc0912 <52129972+phuocdt23@users.noreply.github.com>
2021-11-30 23:15:19 +0700

    install multer package, input = file, form attribute = multipart/form-data, create router dealling with upload file, set destination stored file

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,33 @@
 const express = require('express');
-
+const db = require('../data/database');
+// initialize multer package
 const router = express.Router();
-
-router.get('/', function(req, res) {
+const multer = require('multer');
+//
+const storageCofig = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'images');
+  },
+  filename: function (res, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+})
+const upload = multer({ storage: storageCofig });
+router.get('/', function (req, res) {
   res.render('profiles');
 });
 
-router.get('/new-user', function(req, res) {
+router.get('/new-user', function (req, res) {
   res.render('new-user');
 });
+// add upload additional middleware "upload" from multer package.
+// uses: const upload = multer();
+router.post('/profiles', upload.single('image'), async function (req, res) {
+  const uploadedImage = req.file;
+  const userData = req.body;
+  console.log(uploadedImage);
+  console.log(userData);
+  res.redirect('/');
+})
 
 module.exports = router;
